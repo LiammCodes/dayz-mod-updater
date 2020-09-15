3c4374cb345e0549f66b6f2616bce2f8d150b86c
Liam <61328760+LiammCodes@users.noreply.github.com>
2020-09-15 16:08:17 -0300

    Fixed bug that only overwrote old mods

--- start_update.py
+++ start_update.py
@@ -3,6 +3,7 @@
 # September 13, 2020
 
 import os
+import glob
 import subprocess
 import distutils.core
 
@@ -10,7 +11,7 @@ import distutils.core
 # (make sure there are two backslashes at the end of path as shown below)
 dayz_game_path = "C:\Program Files (x86)\Steam\steamapps\common\DayZ\\"
 dayz_server_path = "C:\Program Files (x86)\Steam\Steamapps\common\DayZServer\\"
-bat_filename = "start.bat"
+bat_filename = "start_mod_server.bat"
 
 # opens program
 def open_program(path):
@@ -22,15 +23,19 @@ def close_program(p):
 
 # start DayZ to Update Mods
 print("Updating mods...")
-dayz = open_program(dayz_game_path + "DayZLauncher.exe")
+dayz = open_program(os.path.join(dayz_game_path, "DayZLauncher.exe"))
 input("Hit any key once mods complete...")
 
 # close DayZ
 close_program(dayz)
 
+# remove old mods from server
+for mod in glob.glob(os.path.join(dayz_server_path, "@*")):
+    distutils.dir_util.remove_tree(mod)
+
 # copy files from !Workshop to server location
 print("Copying mods to server...")
-distutils.dir_util.copy_tree(dayz_game_path + "!Workshop", dayz_server_path)
+distutils.dir_util.copy_tree(os.path.join(dayz_game_path, "!Workshop"), dayz_server_path)
 
 # cleanup
 os.rmdir(dayz_server_path + "!DO_NOT_CHANGE_FILES_IN_THESE_FOLDERS")
